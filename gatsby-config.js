497ebe3eb4a17084ef6af9eb92ba730fc08ccfba
Arjun sn <snarjun92@gmail.com>
2019-06-02 23:14:19 +0530

    clean up assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,13 +58,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Arjun's Portfolio`,
+        short_name: `Arjun`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.jpeg`,
       },
     },
     `gatsby-plugin-offline`,
